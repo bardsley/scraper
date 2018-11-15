676f03b637c5e51a991edcdb1401ff54c42fda21
Adam Bardsley <adam.bardsley@gmail.com>
2018-11-15 12:39:31 +0000

    Now gets events from a listing page - Gets basic detaails - hardcoded to UWSU

--- scrape.rb
+++ scrape.rb
@@ -20,6 +20,27 @@ puts "Found : #{potential_event_page_links.size} potential events_pages"
 
 potential_event_page_links.each { |link| puts "#{link.text.strip}: #{link.href}"  }
 
-#using the first link for speeds
-#
+# Using the first link for speeds
 
+page_to_scrap = potential_event_page_links.first.click
+puts ""
+puts "#{page_to_scrap.title.strip}"
+puts "-" * page_to_scrap.title.strip.size
+
+event_pages = page_to_scrap.search(".event_item")
+puts "Found #{event_pages.size} events"
+
+
+events = event_pages.map do |event|
+  name_elm = event.css("a.msl_event_name")
+  name = name_elm.text
+  link_path = name_elm.first['href']
+  link_path = union_url + link_path unless /\/\//.match link_path
+  location = event.css(".msl_event_location").text
+  description = event.css(".msl_event_description").text
+  puts "About to process #{link_path}"
+  event_page = a.get(link_path)
+  {name: name, location: location, description: description, webpage: link_path}
+end
+
+puts events
