9577cabe0a404dddd1ff9ab7b0676b43018daf68
Adam Bardsley <adam.bardsley@gmail.com>
2018-11-15 11:52:38 +0000

    Better way of getting links

--- scrape.rb
+++ scrape.rb
@@ -10,11 +10,16 @@ a = Mechanize.new { |agent|
   agent.user_agent_alias = 'Mac Safari'
   agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 }
+union_url = "https://www.uwsu.com"
+homepage = a.get(union_url)
+potential_event_page_links = homepage.links_with(href: /event/)
+potential_event_page_links += homepage.links_with(text: /event/i)
+potential_event_page_links.uniq! { |link| link.href }
 
-a.get('https://keelesu.com/') do |page|
-  page.links.each do |link|
-    text = link.text.strip
-    next unless text.length > 0
-    puts text
-  end
-end
+puts "Found : #{potential_event_page_links.size} potential events_pages"
+
+potential_event_page_links.each { |link| puts "#{link.text.strip}: #{link.href}"  }
+
+#using the first link for speeds
+#
+
